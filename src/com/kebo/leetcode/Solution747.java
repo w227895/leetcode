886ca9c0248aa4b2d484bb97f7524af312c273cb
Administrator <249183617@qq.com>
2019-07-15 19:56:42 +0800

    Initial commit

---
+++ src/com/kebo/leetcode/Solution747.java
@@ -0,0 +1,12 @@
+package com.kebo.leetcode;
+
+/**
+ * @description: 至少是其他数字两倍的最大数
+ * @Author: kb
+ * @Date: 2019-07-15 19:53
+ */
+public class Solution747 {
+    public int dominantIndex(int[] nums) {
+
+    }
+}
