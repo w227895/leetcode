99a414d9aa1a8bb29bad61e654434889b0449c38
Administrator <249183617@qq.com>
2019-07-15 20:16:09 +0800

    至少是其他数字两倍的最大数

--- src/com/kebo/leetcode/Solution747.java
+++ src/com/kebo/leetcode/Solution747.java
@@ -6,7 +6,19 @@ package com.kebo.leetcode;
  * @Date: 2019-07-15 19:53
  */
 public class Solution747 {
-    public int dominantIndex(int[] nums) {
-
+    public static int dominantIndex(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > nums[index])
+                index= i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i != index) {
+                if (nums[i] * 2 > nums[index])
+                    return -1;
+            }
+        }
+        return index;
     }
+
 }
